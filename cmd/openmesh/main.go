94e10f5ba1db7f8d8c21bee7b6c005f49cc3671c
xybingbing <xybingbing@live.com>
2026-06-15 20:15:40 +0800

    init: add openmesh CLI entrypoint

---
+++ cmd/openmesh/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/xybingbing/openmesh/internal/agent"
+	"github.com/xybingbing/openmesh/internal/controller"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(2)
+	}
+
+	ctx := context.Background()
+	var err error
+
+	switch os.Args[1] {
+	case "controller":
+		err = runController(ctx, os.Args[2:])
+	case "agent":
+		err = runAgent(ctx, os.Args[2:])
+	case "version":
+		fmt.Println("openmesh dev")
+	default:
+		usage()
+		os.Exit(2)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Println(`openmesh
+
+Usage:
+  openmesh controller --listen :8080 --data ./openmesh.json --token dev-token
+  openmesh agent register --controller http://127.0.0.1:8080 --token dev-token --name node-a --public-key <key>
+  openmesh agent config --controller http://127.0.0.1:8080 --token dev-token --node-id <id>
+  openmesh version`)
+}
+
+func runController(ctx context.Context, args []string) error {
+	fs := flag.NewFlagSet("controller", flag.ContinueOnError)
+	listen := fs.String("listen", ":8080", "HTTP listen address")
+	data := fs.String("data", "openmesh.json", "data file path")
+	token := fs.String("token", "", "API token")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+	return controller.Run(ctx, controller.Config{Listen: *listen, DataPath: *data, Token: *token})
+}
+
+func runAgent(ctx context.Context, args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("missing agent subcommand")
+	}
+	switch args[0] {
+	case "register":
+		fs := flag.NewFlagSet("agent register", flag.ContinueOnError)
+		controllerURL := fs.String("controller", "", "controller URL")
+		token := fs.String("token", "", "API token")
+		name := fs.String("name", "", "node name")
+		publicKey := fs.String("public-key", "", "WireGuard public key")
+		endpoint := fs.String("endpoint", "", "optional public endpoint")
+		if err := fs.Parse(args[1:]); err != nil {
+			return err
+		}
+		return agent.Register(ctx, agent.RegisterConfig{ControllerURL: *controllerURL, Token: *token, Name: *name, PublicKey: *publicKey, Endpoint: *endpoint}, os.Stdout)
+	case "config":
+		fs := flag.NewFlagSet("agent config", flag.ContinueOnError)
+		controllerURL := fs.String("controller", "", "controller URL")
+		token := fs.String("token", "", "API token")
+		nodeID := fs.String("node-id", "", "node id")
+		if err := fs.Parse(args[1:]); err != nil {
+			return err
+		}
+		return agent.Config(ctx, agent.ConfigConfig{ControllerURL: *controllerURL, Token: *token, NodeID: *nodeID}, os.Stdout)
+	default:
+		return fmt.Errorf("unknown agent subcommand %q", args[0])
+	}
+}
